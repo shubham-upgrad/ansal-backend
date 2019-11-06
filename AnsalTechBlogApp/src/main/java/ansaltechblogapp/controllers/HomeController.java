ebd92531be7ecc4cea839735ee43a65ba86861f5
shubham-upgrad <shubham.sharma@upgrad.com>
2019-11-06 21:36:39 +0530

    Home Page for techblog app(Hardcoded)

--- AnsalTechBlogApp/src/main/java/ansaltechblogapp/controllers/HomeController.java
+++ AnsalTechBlogApp/src/main/java/ansaltechblogapp/controllers/HomeController.java
@@ -1,12 +1,38 @@
 package ansaltechblogapp.controllers;
 
+import ansaltechblogapp.models.Post;
+import ansaltechblogapp.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 @Controller // A controller is a Component
 public class HomeController {
+    @Autowired
+    PostService postService;
+
+
+
+
+    /*** This method is supposed to pass posts(plural) to the "index"
+     *  1. It must create Posts that are to be passed
+     *  2. It must pass all the posts at once...
+     *          - It will create an arraylist of posts
+     *  4. Pass that arraylist to the index
+     *          - For this...there is a class called Model(Not the model Post...it is a class)
+     *          - Model is a class that helps you to send data to your views
+     * @return view-name
+     */
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        ArrayList<Post> posts=postService.getAllPosts();
+        model.addAttribute("list_of_posts",posts);
+
+        // list_of_posts is the name using which your index page will access the list
         return "index";
     }
 }
---
+++ AnsalTechBlogApp/src/main/java/ansaltechblogapp/services/PostService.java
@@ -0,0 +1,24 @@
+package ansaltechblogapp.services;
+
+import ansaltechblogapp.models.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Service
+public class PostService {
+    public ArrayList<Post> getAllPosts(){
+        Post p1=new Post("Post 1","Body of Post 1",new Date());
+        Post p2=new Post("Post 2","Body of Post 2",new Date());
+        Post p3=new Post("Post 3","Body of Post 3",new Date());
+        // Creating list of posts
+        ArrayList<Post> posts=new ArrayList<>();
+
+        posts.add(p1);
+        posts.add(p2);
+        posts.add(p3);
+        // Passing list of posts to the index using Model
+        return posts;
+    }
+}
